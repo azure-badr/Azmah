106f29319ef84a76d4145deffd8df9296ba2591b
itsbadr <badrkmc@gmail.com>
2021-09-02 20:30:20 +0500

    add a command to post confession directly

---
+++ commands/post.js
@@ -0,0 +1,85 @@
+const { getConfessionNumber, getConfessionByConfessionId, addConfessionAndUpdate, addConfession, updateConfessionNumber } = require("../utils/config");
+const { confessionsChannelId } = require("../config.json");
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageActionRow, MessageButton } = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("post")
+        .setDescription("Post a confession")
+        .setDefaultPermission(false)
+        .addStringOption((option) =>
+            option
+                .setName("message")
+                .setDescription("The confession message")
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName("replyto")
+                .setDescription("Reply to a confession with their confession number")
+                .setRequired(false)
+        ),
+
+    async execute(interaction) {
+        const replyValue = interaction.options.get("replyto")?.value;
+        if (replyValue && !getConfessionByConfessionId(Number(replyValue))) {
+            interaction.reply({ content: "This confession does not exist! 📭" });
+            return;
+        }
+
+        let confessionNumber = getConfessionNumber();
+        const confessionsChannel = interaction.guild.channels.cache.get(confessionsChannelId);
+        const confessionMessage = {
+            content: `${interaction.options.get("message").value}`,
+            components: [
+                new MessageActionRow().addComponents(
+                    new MessageButton()
+                        .setCustomId((confessionNumber++).toString())
+                        .setLabel(`Confession ${confessionNumber}`)
+                        .setStyle("SECONDARY")
+                        .setDisabled()
+                ),
+            ],
+        }
+
+        let sentConfessionMessage;
+        if (replyValue) {
+            let confessionRepliedToId = getConfessionByConfessionId(Number(replyValue)).id;
+            confessionsChannel.messages.fetch(confessionRepliedToId)
+                .then(async message => {
+                    sentConfessionMessage = await message.reply(confessionMessage);
+                    addConfession(
+                        {
+                            postedByModerator: true,
+                            confessionNumber,
+                            confessorName: interaction.user.username,
+                            confessorId: interaction.user.id,
+                            sentConfessionMessageUrl: sentConfessionMessage.url,
+                            sentConfessionMessageId: sentConfessionMessage.id,
+                            confessionMessageContent: interaction.options.get("message").value,
+                            confessionReplyTo: replyValue
+                        }
+                    );
+                    updateConfessionNumber(confessionNumber);
+                });
+            interaction.reply({ content: "Your confession has been posted" });
+            return;
+        }
+
+        sentConfessionMessage = await confessionsChannel.send(confessionMessage);
+        addConfession(
+            {
+                postedByModerator: true,
+                confessionNumber,
+                confessorName: interaction.user.username,
+                confessorId: interaction.user.id,
+                confessionPostedMessageUrl: sentConfessionMessage.url,
+                confessionPostedMessageId: sentConfessionMessage.id,
+                confessionMessageContent: interaction.options.get("message").value,
+            }
+        );
+        updateConfessionNumber(confessionNumber);
+        interaction.reply({ content: "Your confession has been posted" });
+    },
+}
