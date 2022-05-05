b97f201a0233ced9ed3d0918bcc143a46c905f61
itsbadr <badrkmc@gmail.com>
2022-05-05 18:33:55 +0500

    indentation

--- azmah.js
+++ azmah.js
@@ -17,12 +17,12 @@ for (const file of commandFiles) {
 
 const eventFiles = fs.readdirSync(`./events`).filter(file => file.endsWith(".js"));
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 client.on("interactionCreate", async (interaction) => {
