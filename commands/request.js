40849592ccbe3f86230cdb65f6fabccb685662ec
Badr <badrkmc@gmail.com>
2025-05-18 17:00:05 +0500

    feat: add confession requesting

--- commands/request.js
+++ commands/request.js
@@ -1,68 +1,56 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { inlineCode } = require("discord.js");
+const { inlineCode, DiscordAPIError } = require("discord.js");
 const { getConfession, decrypt } = require("../utils/config");
 
+const { ownerRoleId } = require("../config");
+
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("request")
-    .setDescription("Request confession data by its number or message ID")
-    .setDefaultMemberPermissions('0')
-    .addStringOption(option =>
-      option.setName("number")
-        .setDescription("Get confession data by its number")
-        .setRequired(false)
-    )
-    .addStringOption(option =>
-      option.setName("messageid")
-        .setDescription("Get confession data by its message ID")
-        .setRequired(false)
-    ),
-  async execute(interaction) {
-    if (interaction.options.get("number")?.value && interaction.options.get("messageid")?.value) {
-      interaction.reply({ content: "You can only specify one option at a time!", ephemeral: true });
+  data: { name: "request" },
+  async execute(message, ...content) {
+    if (!message.inGuild()) return;
+
+    if (ownerRoleId === undefined) return;
+    if (!message.member.roles.cache.hasAny(ownerRoleId)) return;
+    
+    if (content.length > 1) {
+      await message.channel.send("Invalid use of the command. Specify either the message ID of the confession in the approvals channel, or the confession number");
       return;
     }
 
-    if (interaction.options.get("number")?.value) {
-      const confession = await getConfession({ number: Number(interaction.options.get("number").value) });
-      if (confession === null) {
-        interaction.reply({
-          content: "A confession with this number does not exist",
-          ephemeral: true
-        });
+    const number = content[0];
+    if (!/^\d+$/.test(number)) {
+      await message.channel.send("Specify a confession number or a message ID found in the approvals channel");
+      return;
+    }
+
+      let confession = null;
+      confession = await getConfession({ number });
+      if (!confession) {
+        confession = await getConfession({ approval_message_id: number });
+      }
+
+      if (!confession) {
+        await message.channel.send("A confession with this number does not exist");
         return;
       }
 
-      interaction.reply({
-        content:
-          `Confessor ID: ${inlineCode(decrypt(confession.confessor_id))}
-Confessor name: ${inlineCode(decrypt(confession.confessor_name))}
-Confession message ID: ${inlineCode(confession.message_id)}
-Confession message ID on #confessions: ${inlineCode(confession.approved_message_id)}
-Please compare and verify the requested confession message ID in #confessions with the one listed above`
-      });
-      return;
-    }
-
-    if (interaction.options.get("messageid")?.value) {
-      const confession = await getConfession({ approval_message_id: interaction.options.get("messageid").value });
-      if (confession === null) {
-        interaction.reply({
-          content: "A confession with this message ID does not exist",
-          ephemeral: true
-        });
-        return;
+      try {
+        await message.member.send([
+          `Confessor ID: ${inlineCode(decrypt(confession.confessor_id))}`,
+          `Confessor name: ${inlineCode(decrypt(confession.confessor_name))}`,
+          `Confession message ID: ${inlineCode(confession.message_id)}`,
+          `Confession message ID on #confessions: ${inlineCode(confession.approved_message_id)}`,
+          `Please compare and verify the requested confession message ID in #confessions with the one listed above`
+        ].join('\n'));
+        
+        await message.channel.send("The confession has been sent in your DMs. Please check");
+      } catch (error) {
+        if (error instanceof DiscordAPIError) {
+          await message.channel.send("Your DMs need to be open for me to send the confession!");
+          return;
+        }
+        
+        await message.channel.send("Something went wrong!");
+        console.log(error);
       }
-
-      interaction.reply({
-        content:
-          `Confessor ID: ${inlineCode(decrypt(confession.confessor_id))}
-Confessor name: ${inlineCode(decrypt(confession.confessor_name))}
-Confession message ID: ${inlineCode(confession.message_id)}
-Confession message ID on #confessions-approval: ${inlineCode(confession.approval_message_id)}
-Please compare and verify the requested confession message ID in #confessions-approval with the one listed above`
-      });
-      return;
     }
-  }
 }
